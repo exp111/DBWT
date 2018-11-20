805d7663a5e1ece71ca61df768ad52d218c58843
T N <theexp111@gmail.com>
2018-11-20 13:46:10 +0100

    Started mvc

---
+++ P3/Controllers/ProdukteController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using P3.Models;
+
+namespace P3.Controllers
+{
+    public class ProdukteController : Controller
+    {
+        // GET: Produkte
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+	    public ActionResult Details(int id)
+	    {
+		    if (id == null)
+		    {
+			    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+		    }
+
+		    Mahlzeit m = new Mahlzeit()
+		    {
+			    ID = id
+		    };
+		    return View(m);
+	    }
+    }
+}
---
+++ P3/Models/Mahlzeit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace P3.Models
+{
+	public class Mahlzeit
+	{
+		public int ID = 0;
+		public string Name = "no u";
+	}
+}
