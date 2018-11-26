e1026e02716245572007f9c6db5fc68e91d1b3c1
T N <theexp111@gmail.com>
2018-11-26 11:31:30 +0100

    Started Login

---
+++ P3/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebPages;
+using P3.Models;
+
+namespace P3.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+			Login login = new Login(){LoggedIn = false};
+	        bool isPost = Request.HttpMethod == "POST";
+			if (isPost)
+	        {
+		        if (Request["loginName"] == "a" || Request["loginName"].IsEmpty() || Request["loginPassword"].IsEmpty())
+		        {
+			        ModelState.AddModelError("Error", "Das hat nicht geklappt! Bitte versuchen Sie es erneut.");
+			        login.Failed = true;
+			        return View(login);
+		        }
+		        login.LoggedIn = true;
+		        login.Username = Request["loginName"].ToString();
+		        login.Type = "Student";
+	        }
+            return View(login);
+        }
+    }
+}
---
+++ P3/Models/Login.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace P3.Models
+{
+	public class Login
+	{
+		public bool LoggedIn { get; set; }
+		public string Username { get; set; }
+		public string Type { get; set; }
+		public bool Failed { get; set; }
+	}
+}
